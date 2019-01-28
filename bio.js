8a41299a8413465a3b3ed4b776600ecea6337065
dainryoo <dainryoo@gmail.com>
2019-01-28 00:28:11 -0500

    Finished stat generation and added bio

---
+++ bio.js
@@ -0,0 +1,28 @@
+function bio() {
+    return hello() + farewell();
+}
+
+function hello() {
+    var date = new Date();
+    var hour = date.getHours();
+
+    var intro = "";
+    if (hour < 4) {
+        intro = "Good night! Aren't you up late? ";
+    } else if (hour < 13) {
+        intro = "Good morning! ";
+    } else if (hour < 17) {
+        intro = "Good afternoon! ";
+    } else {
+        intro = "Good evening! "
+    }
+
+    var name = "";
+    name = "My name is " + firstName() + ". ";
+
+    return intro + name;
+}
+
+function farewell() {
+    return "I love you. ";
+}
